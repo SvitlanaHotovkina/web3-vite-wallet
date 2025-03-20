2ef70d4da16d03b890408a0ec3eaaed26c802d8c
nigyl.pach <nigyl.pach@gmail.com>
2025-03-20 16:53:24 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SmaCrossover from "./pages/SmaCrossover/components/SmaCrossover";
-import DatabasePage from "./pages/DataBase/components/DatabasePage";
-
 function App() {
-  return (
-    <Router>
-      <div className="min-h-screen bg-gray-900 text-white">
-        <Routes>
-          {/* Головна сторінка */}
-          <Route path="/" element={<SmaCrossover />} />
-          {/* Сторінка з базою даних */}
-          <Route path="/db" element={<DatabasePage />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+  return "init";
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App"; // ✅ Видаляємо `.tsx`, TypeScript сам знайде файл
-import "./index.css"; // ✅ Должно быть обязательно
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+<React.StrictMode>
+  <App />
+</React.StrictMode>;
